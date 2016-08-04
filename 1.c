cd7df2376c785f7c9ed5e72f0f803f5a396b32c2
fengxinlinux <903087053@qq.com>
2016-08-04 16:58:31 +0800

    1

---
+++ 1.c
@@ -0,0 +1,17 @@
+/*************************************************************************
+	> File Name: 1.c
+	> Author: FengXin
+	> Mail: fengxinlinux@gmail.com
+	> Created Time: 2016年08月04日 星期四 16时58分17秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+#include<sys/signal.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<sys/stat.h>
+weq
