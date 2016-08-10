4689cd4027b1ef629627b2f78147f7ea560cc99e
fengxinlinux <903087053@qq.com>
2016-08-10 18:10:04 +0800

    服务器

--- server.c
+++ server.c
@@ -1,9 +1,9 @@
 /*************************************************************************
-	> File Name: server.c
-	> Author: FengXin
-	> Mail: fengxinlinux@gmail.com
-	> Created Time: 2016年08月08日 星期一 14时13分13秒
- ************************************************************************/
+> File Name: server.c
+> Author: FengXin
+> Mail: fengxinlinux@gmail.com
+> Created Time: 2016年08月08日 星期一 14时13分13秒
+************************************************************************/
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -18,6 +18,8 @@
 #include<netinet/in.h>
 #include<arpa/inet.h>
 #include<sys/epoll.h>
+#include<fcntl.h>
+#include<time.h>
 #include"message.h"
 
 #define PORT   6666      //服务器端口
@@ -25,16 +27,30 @@
 #define EPOLL_SIZE    1024   //监听的客户端的最大数目
 #define BUF_MAX     257     //读取缓存区最大字节 
 
-type struct cli_data
+time_t timep;     //记录当前时间
+
+typedef struct cli_data   //记录在线用户信息的结构体
 {
     int  cli_fd;    //记录连接的用户套接字
     char username[21];   //记录账号，最大20位
     char ip[16];    //记录ip地址
     struct cli_data* next;
 
-
-
 }cli_data;
+
+typedef struct user      //记录用户账号信息的结构体
+{
+    char username[21];
+    char pwd[21];
+
+}user;
+char* my_time() //输出当前时间
+{
+    
+    time(&timep);
+    char* p=ctime(&timep);
+    return p;
+}
 
 cli_data* create()  //创建一个带头节点的链表，储存已连接的用户
 {
@@ -51,7 +67,7 @@ void insert(cli_data* temp)   //插入链表
     new->next=NULL;
     new->next=temp->next;
     temp->next=new;
-    
+
 }
 void delete(cli_data* head,int cli_fd)   //链表的删除
 {
@@ -62,7 +78,7 @@ void delete(cli_data* head,int cli_fd)   //链表的删除
     {
         last=temp;
         temp=temp->next;
-        
+
     }
     if(temp->cli_fd==cli_fd)
     {
@@ -74,18 +90,109 @@ void delete(cli_data* head,int cli_fd)   //链表的删除
 
 void my_err(char* string)
 {
-    perror("string");
+    perror(string);
     exit(1);
+}
+
+void send_message(struct message recv_buf,int conn_fd)   //向客户端发送信息
+{
+    struct message send_buf;
+    send_buf=recv_buf;
+    int n=send_buf.n;
+    int fd;
+    struct user user_data;
+    int ret=0;  //用来表示注册账号是否存在或账号密码信息是否匹配
+    FILE* fp;
+    fp=fopen("passwd.txt","a+");
+    fclose(fp);
+    switch(n)
+    {
+        case 0: //注册
+        
+        if((fd=open("passwd.txt",O_RDWR|O_APPEND,777))<0)
+        {
+            my_err("open");
+        }
+        
+
+        while(read(fd,&user_data,sizeof(struct user))>0)
+        {
+            if(strcmp(user_data.username,send_buf.username)==0)
+            {
+                send_buf.n=-2;
+                ret=1;
+                break;
+            }
+
+        }
+        if(!ret)
+        {
+            strcpy(user_data.username,recv_buf.username);
+            strcpy(user_data.pwd,recv_buf.pwd1);
+            send_buf.n=22;
+    
+            if(write(fd,&user_data,sizeof(struct user))<0)
+            {
+                my_err("write");
+            }
+
+            printf("a user register,username:%s,time:%s",user_data.username,my_time());
+
+            close(fd);
+        }
+        if(send(conn_fd,&send_buf,sizeof(struct message),0)<0)
+        {
+            my_err("send");
+        }
+        break;
+        case 1:  //登陆
+        if((fd=open("passwd.txt",O_RDONLY))<0)
+        {
+            my_err("open");
+
+        }
+        while(read(fd,&user_data,sizeof(struct user))>0)
+        {
+        
+            if(strcmp(user_data.username,send_buf.username)==0)
+            {
+                if(strcmp(user_data.pwd,send_buf.pwd1)==0)
+                {
+                    ret=1;
+                    break;
+                }
+                
+            }
+        
+        }
+        if(ret)
+        send_buf.n=11;
+        else
+        send_buf.n=-1;
+        if(send(conn_fd,&send_buf,sizeof(struct message),0)<0)
+        {
+            my_err("send");
+        }
+        if(send_buf.n==11)
+        printf("a user is login,username:%s,time:%s",send_buf.username,my_time());
+        break;
+
+
+
+
+    }
+
 }
 
 int main()
 {
     int sock_fd,conn_fd;  //储存套接字
     struct sockaddr_in cli_addr,serv_addr;
-    struct message recv_buf  //读取缓存区
+    struct message recv_buf;  //读取缓存区
     socklen_t cli_len;
     int epollfd;
-    struct epoll_event* event;
+    struct epoll_event  event;
+    struct epoll_event*  events;
     struct cli_data* head=NULL,*temp=NULL;
 
 
@@ -109,7 +216,9 @@ int main()
     serv_addr.sin_port=htons(PORT);
     serv_addr.sin_addr.s_addr=htonl(INADDR_ANY);
 
-    if(bind(sock_fd,(struct sockaddr*)serv_addr,sizeof(struct sockaddr_in))<0)
+    
+
+    if(bind(sock_fd,(struct sockaddr*)&serv_addr,sizeof(struct sockaddr_in))<0)
     {
         my_err("bind");
     }
@@ -119,72 +228,84 @@ int main()
     {
         my_err("listen");
     }
-    
+
 
     cli_len=sizeof(struct sockaddr_in);
-    event=(struct epoll_event*)malloc(sizeof(struct epoll_event)*EPOLL_SIZE);
-   
+    events=(struct epoll_event*)malloc(sizeof(struct epoll_event)*EPOLL_SIZE);
+
     //创建一个监听描述符epoll,并将监听套接字加入
     epollfd=epoll_create(EPOLL_SIZE);
     if(epollfd==-1)
     {
         my_err("epoll_create");
     }
+   
+    event.events = EPOLLIN;
+    event.data.fd = sock_fd;
+
     if(epoll_ctl(epollfd,EPOLL_CTL_ADD,sock_fd,&event)<0)
     {
         my_err("epoll_ctl");
     }
 
-   
+
     head=create();  //创建链表
     temp=head;
 
     while(1)
     {
+        
         int sum=0,i;
-        sum=epoll_wait(epollfd,event,EPOLL_SIZE,-1);
+        sum=epoll_wait(epollfd,events,EPOLL_SIZE,-1);
+    
         for(i=0;i<sum;i++)
         {
-            if(event[i].data.fd==sock_fd)    //有请求连接
+            if(events[i].data.fd==sock_fd)    //有请求连接
             {
+                
                 insert(temp);
                 temp=temp->next;
-                cli_fd=accept(sock_fd,(struct sockaddr*)&cli_addr,&cli_len);
-                if(cli_fd<0)
+                conn_fd=accept(sock_fd,(struct sockaddr*)&cli_addr,&cli_len);
+        
+        
+                if(conn_fd<0)
                 {
                     my_err("accept");
                 }
-                temp->cli_fd=cli_fd;
-                temp->ip=inet_ntoa(cli_addr);
+                temp->cli_fd=conn_fd;
+                strcpy(temp->ip,inet_ntoa(cli_addr.sin_addr));
 
-                if(epoll_ctl(epollfd,EPOLL_CTL_ADD,cli_fd,&event)<0)   //将新连接的套接字加入监听
+                event.events = EPOLLIN | EPOLLRDHUP;
+                event.data.fd = conn_fd;
+
+                if(epoll_ctl(epollfd,EPOLL_CTL_ADD,conn_fd,&event)<0)   //将新连接的套接字加入监听
                 {
                     my_err("epoll_ctl");
                 }
-                printf("a connet is connected,ip is %s\n",inet_ntoa(cli_addr));
-                
+                printf("a connet is connected,ip is %s,time:%s",inet_ntoa(cli_addr.sin_addr),my_time());
 
 
-                
+
+
             }
-            else if(event[i].events&EPOLLIN)    //有可读的套接字
+            else if(events[i].events&EPOLLIN)    //有可读的套接字
             {
-               memset(&recv_buf,0,sizeof(struct message));  
-               if(recv(cli_fd,&recv_buf,sizeof(struct message))<0)
-                   {
-                       my_err("recv");
-                   }
+                memset(&recv_buf,0,sizeof(struct message));  
+                if(recv(conn_fd,&recv_buf,sizeof(struct message),0)<0)
+                {
+                    my_err("recv");
+                }
+            
+                send_message(recv_buf,conn_fd);
 
 
-
-
-                
             }
-            else if(event[i].events&EPOLLRDHUP)
+            else if(events[i].events&EPOLLRDHUP)
             {
-                delete(head,event[i].data.fd);
-                close(event[i].data.fd);
-                
+                delete(head,events[i].data.fd);
+                printf("a connet is quit,ip is %s,time:%s",inet_ntoa(cli_addr.sin_addr),my_time());
+                close(events[i].data.fd);
+
             }
             else
             continue;
@@ -197,9 +318,5 @@ int main()
     }
 
     return 0;
-
-
-
-
 
 }
